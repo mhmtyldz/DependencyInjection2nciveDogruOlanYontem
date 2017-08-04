081d80c2887f8a9394d0db2812fd6a74b6197256
Mahmut Yıldız <yldzmahmut0@gmail.com>
2017-08-04 15:38:00 +0300

    DependencyInjection2.VeDogruolanYontem

---
+++ DependencyInjection2Yonetimim/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace DependencyInjection2Yonetimim
+{
+    public interface IFlightProvider
+    {
+        void GetFlight(string sehir);
+    }
+
+     class PegasusProvider : IFlightProvider
+    {
+        public void GetFlight(string sehir)
+        {
+            var result = "18:30 Dubai";
+            Console.WriteLine("Uçuş : " + result + " uçuşumuz vardır.");
+        }
+    }
+
+     class EmiratesProvider : IFlightProvider
+    {
+        public void GetFlight(string sehir)
+        {
+            var result = "18:30 Budapest";
+            Console.WriteLine("Uçuş : " + result + " uçuşumuz vardır.");
+        }
+    }
+
+    class UcanAdamFactory
+    {
+        public IFlightProvider CreateProvider(string providerType)
+        {
+            IFlightProvider provider = null;
+            switch (providerType)
+            {
+                case "Emirates": provider = new EmiratesProvider();
+                    break;
+                case "Pegasus": provider = new PegasusProvider();
+                    break;
+            }
+
+            return provider;
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var providerType = ConfigurationManager.AppSettings["providerType"];
+            var factory = new UcanAdamFactory();
+            var provider = factory.CreateProvider(providerType);
+            provider.GetFlight("istanbul");
+            
+
+
+
+        }
+    }
+}
